cafb900dbe50382f2cb9f51855adfabd6d5d5155
Zaben-dev <patrykdmitruk227@gmail.com>
2021-03-01 17:17:03 +0100

    clean react template

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Column } from './api/models';
-import logo from './logo.svg';
 import getColumns from './api/getColumns';
-import './App.css';
 import server from './api/mockServer';
 
 server();
@@ -16,24 +14,7 @@ function App() {
     });
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
